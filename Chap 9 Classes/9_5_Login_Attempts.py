2dbcb13fce3be3fe40999c74c6b5846881d0c5fb
Diggerfdf <diggerfdf@gmail.com>
2018-02-17 19:55:17 -0200

    teste

--- Chap 9 Classes/9_5_Login_Attempts.py
+++ Chap 9 Classes/9_5_Login_Attempts.py
@@ -8,6 +8,7 @@ class User():
         self.username = username.title()
         self.email = email
         self.location = location.title()
+        self.login_attempts = 0
 
     def describe_user(self):
         """Display a summary of the user's information"""
@@ -20,11 +21,25 @@ class User():
         """Display a personalized greeting to the user"""
         print("\nWelcome Back, {}!".format(self.username))
 
+    def increment_login_attempts(self):
+        """Increment the value of login_attempts"""
+        self.login_attempts += 1
 
-fabiano = User('fAbIano', 'feRReiRA', 'digger', 'diggerfdf@gmail.com', 'RIbeirÃO PIRes')
+    def reset_login_attempts(self):
+        """reset_login_attempts to 0"""
+        self.login_attempts = 0
+
+
+fabiano = User('fAbIano', 'feRReiRA', 'digger', 'diggerfdf@gmail.com', 'Sp')
 fabiano.describe_user()
 fabiano.greet_user()
 
-otavio = User('otAvio', 'aSSis', 'oTacOn', 'otavio.marquesassis@gmail.com', 'Ribeirão Pires')
-otavio.describe_user()
-otavio.greet_user()
+print("\nMaking 3 login attempts...")
+fabiano.increment_login_attempts()
+fabiano.increment_login_attempts()
+fabiano.increment_login_attempts()
+print("  Login Attempts: " + str(fabiano.login_attempts))
+
+print("Resetting login attempts...")
+fabiano.reset_login_attempts()
+print("  Login Attempts: " + str(fabiano.login_attempts))
