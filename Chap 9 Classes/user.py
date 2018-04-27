72b8cb47cb79a51eb53bcd184e835f0eaeaf26b6
Diggerfdf <diggerfdf@gmail.com>
2018-04-27 18:38:51 -0300

    Updating Files

--- Chap 9 Classes/user.py
+++ Chap 9 Classes/user.py
@@ -31,27 +31,3 @@ class User():
     def reset_login_attempts(self):
         """Reset login_attempts to 0."""
         self.login_attempts = 0
-
-
-class Admin(User):
-    """A user with administrative privileges."""
-
-    def __init__(self, first_name, last_name, username, email, location):
-        """Initialize the admin."""
-        super().__init__(first_name, last_name, username, email, location)
-
-        # Initialize an empty set of privileges.
-        self.privileges = Privileges([])
-
-
-class Privileges():
-    """Stores privileges associated with an Admin account."""
-
-    def __init__(self, privileges):
-        """Initialize the privileges object."""
-        self.privilege = privileges
-
-    def show_privileges(self):
-        """Display the privileges this administrator has."""
-        for privilege in self.privileges:
-            print("- " + privilege)
